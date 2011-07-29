f8e04983ff71c26c6cf2d6bc641a8da871d2a5d4
Ayan Dave <daveayan@gmail.com>
2011-07-28 21:16:06 -0400

    Added more Reflectiion methods

--- src/main/java/mirage/ReflectionUtils.java
+++ src/main/java/mirage/ReflectionUtils.java
@@ -1,12 +1,108 @@
 package mirage;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class ReflectionUtils {
+	public static List<Field> getAllFieldsIn(Object object) {
+		List<Field> classes = new ArrayList<Field>();
+		classes.addAll(Arrays.asList(object.getClass().getDeclaredFields()));
+		Class<?> superclass = object.getClass().getSuperclass();
+		while (superclass != null) {
+			classes.addAll(Arrays.asList(superclass.getDeclaredFields()));
+			superclass = superclass.getSuperclass();
+		}
+
+		return classes;
+	}
+
+	public static List<Field> getAllFieldsSortedAscendingIn(Object object) {
+		List<Field> allFields = getAllFieldsIn(object);
+
+		Comparator<Field> comparator = new Comparator<Field>() {
+			public int compare(Field f1, Field f2) {
+				return f1.getName().trim().compareToIgnoreCase(f2.getName().trim());
+			}
+		};
+		Collections.sort(allFields, comparator);
+		return allFields;
+	}
+
+	public static boolean isAccessible(Field field) {
+		return Modifier.isPublic(field.getModifiers());
+	}
+
+	public static void makeAccessible(Field field) {
+		if (!isAccessible(field)) {
+			field.setAccessible(true);
+		}
+	}
+
+	public static Class<?> asClass(String className) {
+		try {
+			return Class.forName(className);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static Object objectFor(Class<?> clazz) {
+		try {
+			return clazz.newInstance();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static Object objectForClassForcibly(Class<?> clazz) {
+		try {
+			Constructor<?> c = clazz.getDeclaredConstructor();
+			c.setAccessible(true);
+			return c.newInstance();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static Object objectFor(String className) {
+		return objectForClassForcibly(asClass(className));
+	}
+
 	public static Method getMethodFor(Object targetObject, String methodName, Object[] parameters) {
 		return getMethodFor(targetObject.getClass(), methodName, parameters);
 	}
-	
+
 	public static Method getMethodFor(Class<?> targetClass, String methodName, Object[] parameters) {
 		Method[] methods = targetClass.getMethods();
 		for (Method method : methods) {
@@ -20,7 +116,7 @@ public class ReflectionUtils {
 	}
 
 	public static boolean canCast(Class<?>[] to, Object[] from) {
-		if(to == null || from == null)
+		if (to == null || from == null)
 			return false;
 		if (to.length != from.length)
 			return false;
