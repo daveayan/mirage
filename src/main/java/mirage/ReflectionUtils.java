439f4ddbc18529df63b5065f4cee56896eea53a6
Ayan Dave <daveayan@gmail.com>
2011-08-01 18:08:13 -0400

    objectIsOfType method added

--- src/main/java/mirage/ReflectionUtils.java
+++ src/main/java/mirage/ReflectionUtils.java
@@ -11,7 +11,26 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
+
 public class ReflectionUtils {
+	public static boolean objectIsOfType(Class<?> actualClass, Class<?> expectedClass) {
+		return actualClass.getName().trim().equals(expectedClass.getName().trim());
+	}
+	
+	public static boolean objectIsOfType(Object object, Class<?> expectedClass) {
+		return object.getClass().getName().trim().equals(expectedClass.getName().trim());
+	}
+	
+	public static List<Class<?>> classNamesToClasses(List<String> classNames) {
+		return Lists.transform(classNames, new Function<String, Class<?>>() {
+			public Class<?> apply(String type) {
+				return ReflectionUtils.getClassForClassName(type);
+			}
+		});
+	}
+	
 	public static List<Field> getAllFieldsIn(Object object) {
 		List<Field> classes = new ArrayList<Field>();
 		classes.addAll(Arrays.asList(object.getClass().getDeclaredFields()));
@@ -129,6 +148,60 @@ public class ReflectionUtils {
 			}
 		}
 		return true;
+	}
+	
+	public static Object getInstanceOfClassForcibly(String className) {
+		return getInstanceOfClassForcibly(getClassForClassName(className));
+	}
+
+	public static Object getInstanceOfClassForcibly(Class<?> clazz) {
+		try {
+			Constructor<?> c = clazz.getDeclaredConstructor();
+			c.setAccessible(true);
+			return c.newInstance();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static Object getInstanceOfClass(Class<?> clazz) {
+		try {
+			return clazz.newInstance();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static Class<?> getClassForClassName(String className) {
+		try {
+			return Class.forName(className);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	public static boolean canCast(Object to, Object from) {
